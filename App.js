43c9789db4ce3924693aa9ba683ccccff77d13f8
MUTHUPANDI-P <158024739+MUTHUPANDI-P@users.noreply.github.com>
2024-11-28 10:22:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Quiz from './Quiz';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz Application</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
+
